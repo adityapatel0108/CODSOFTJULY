234a3980e434a61f0643453d636fa85584e419b3
Aditya Patel <91675954+adityapatel0108@users.noreply.github.com>
2023-07-28 14:21:33 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int playerInput;
+    cin >> playerInput;
+
+    switch (1)
+    {
+        case 1:  
+        cout<< "Case 1";
+           exit(0); 
+            
+
+        case 2: playerInput< 1;
+        cout<< "Case 2";
+             
+            break;
+    }
+}
